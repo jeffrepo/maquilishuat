141e295e232e52672977bf879863b2fdcbfb1d99
jeffrepo <jeffer.leo93@gmail.com>
2021-02-09 23:15:41 -0600

    campos cuentas

--- wizard/ingresos_diarios.py
+++ wizard/ingresos_diarios.py
@@ -14,7 +14,7 @@ class ingresos_diarios_wizard(models.TransientModel):
 
     fecha_inicio = fields.Date('Fecha inicio')
     fecha_fin = fields.Date('Fecha fin')
-    cuentas_ids = fields.Many2many('account.account','maqui_cuentas_rel','Cuentas')
+    cuentas_ids = fields.Many2many('account.account','maqui_cuentas_rel',string='Cuentas')
     # archivo = fields.Binary('Archivo excel')
 
 
