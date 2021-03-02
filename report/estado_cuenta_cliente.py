444123aa254823377befea8764fe5c736489b447
jeffrepo <jeffer.leo93@gmail.com>
2021-03-01 20:25:24 -0600

    test

--- report/estado_cuenta_cliente.py
+++ report/estado_cuenta_cliente.py
@@ -91,7 +91,7 @@ class ReportEstadoCuentaCliente(models.AbstractModel):
 
 
     def estado_cuenta(self,fecha_inicio,fecha_fin,cliente_id):
-        facturas_ids = self.env['account.invoice'].search([('id','=',cliente_id[0]),('date_invoice','>=', fecha_inicio),('date_invoice','<=',fecha_fin),('type','=','out_invoice'),('state','in',['in_payment','open'])],order="date_invoice asc")
+        facturas_ids = self.env['account.invoice'].search([('id','=',cliente_id[0]),('date_invoice','>=', fecha_inicio),('date_invoice','<=',fecha_fin),('state','in',['in_payment','open'])],order="date_invoice asc")
         datos = {}
         if facturas_ids:
             for f in facturas_ids:
