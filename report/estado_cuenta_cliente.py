aef948ce8671593fa02d043b707b79b0fb38a9e4
jeffrepo <jeffer.leo93@gmail.com>
2021-03-01 20:54:06 -0600

    cliente test

--- report/estado_cuenta_cliente.py
+++ report/estado_cuenta_cliente.py
@@ -91,6 +91,7 @@ class ReportEstadoCuentaCliente(models.AbstractModel):
 
 
     def estado_cuenta(self,fecha_inicio,fecha_fin,cliente_id):
+        logging.warn(cliente_id)
         facturas_ids = self.env['account.invoice'].search([('id','=',cliente_id[0])],order="date_invoice asc")
         datos = {}
         logging.warn(facturas_ids)
