99eeff1b1fdfeda1656520e0be01b4898fee9608
jeffrepo <jeffer.leo93@gmail.com>
2021-03-19 01:07:42 -0600

    abonos proveedores

--- report/abonos_proveedor.py
+++ report/abonos_proveedor.py
@@ -46,38 +46,68 @@ class ReportAbonosProveedor(models.AbstractModel):
     def _get_facturas(self,fecha_inicio,fecha_fin):
         facturas_compra = []
         totales = {'30':0,'mas':0,'total':0}
-        facturas_ids = self.env['account.invoice'].search([('date_invoice','>=', fecha_inicio),('date_invoice','<=',fecha_fin),('type','=','in_invoice'),('state','in',['paid','open'])],order="date_invoice asc")
-
-        if facturas_ids:
+        # facturas_ids = self.env['account.invoice'].search([('date_invoice','>=', fecha_inicio),('date_invoice','<=',fecha_fin),('type','=','in_invoice'),('state','in',['paid','open'])],order="date_invoice asc")
+        pagos_ids = self.env['account.payment'].search([('payment_date','>=', fecha_inicio),('payment_date','<=',fecha_fin),('payment_type','=','outbound'),('state','in',['posted'])],order="payment_date asc")
+        if pagos_ids:
             fecha_hoy = date.today()
-            for factura in facturas_ids:
-                if factura.payment_ids:
-                    for pago in factura.payment_ids:
-                        diferencia_dias = pago.payment_date - factura.date_invoice
-                        dias = diferencia_dias.days
-                        treinta = 0
-                        mas = 0
-                        total = 0
-                        if dias <= 30:
-                            treinta = pago.amount
-                        elif dias > 30:
-                            mas =  pago.amount
-                        total = treinta + mas
-                        pago_dic = {
-                            'fecha_abono': pago.payment_date,
-                            'fecha_comprobante': factura.date_invoice,
-                            'comprobante': pago.communication,
-                            'factura': factura.reference,
-                            'proveedor': factura.partner_id.name,
-                            'cheque': pago.name,
-                            '30': treinta,
-                            'mas': mas,
-                            'total': total,
-                        }
-                        totales['30'] += treinta
-                        totales['mas'] += mas
-                        totales['total'] += total
-                        facturas_compra.append(pago_dic)
+            for pago in pagos_ids:
+                if pago.invoice_ids:
+                    diferencia_dias = pago.payment_date - pago.invoice_ids[0].date_invoice
+                    dias = diferencia_dias.days
+                    treinta = 0
+                    mas = 0
+                    total = 0
+                    if dias <= 30:
+                        treinta = pago.amount
+                    elif dias > 30:
+                        mas =  pago.amount
+                    total = treinta + mas
+                    pago_dic = {
+                        'fecha_abono': pago.payment_date,
+                        'fecha_comprobante': pago.invoice_ids[0].date_invoice,
+                        'comprobante': pago.communication,
+                        'proveedor': pago.partner_id.name,
+                        'cheque': pago.name,
+                        '30': treinta,
+                        'mas': mas,
+                        'total': total,
+                    }
+                    totales['30'] += treinta
+                    totales['mas'] += mas
+                    totales['total'] += total
+                    facturas_compra.append(pago_dic)
+
+        # if facturas_ids:
+        #     fecha_hoy = date.today()
+        #     for factura in facturas_ids:
+        #         if factura.payment_ids:
+        #             for pago in factura.payment_ids:
+        #                 if pago.amount > 0:
+        #                     diferencia_dias = pago.payment_date - factura.date_invoice
+        #                     dias = diferencia_dias.days
+        #                     treinta = 0
+        #                     mas = 0
+        #                     total = 0
+        #                     if dias <= 30:
+        #                         treinta = pago.amount
+        #                     elif dias > 30:
+        #                         mas =  pago.amount
+        #                     total = treinta + mas
+        #                     pago_dic = {
+        #                         'fecha_abono': pago.payment_date,
+        #                         'fecha_comprobante': factura.date_invoice,
+        #                         'comprobante': pago.communication,
+        #                         'factura': factura.reference,
+        #                         'proveedor': factura.partner_id.name,
+        #                         'cheque': pago.name,
+        #                         '30': treinta,
+        #                         'mas': mas,
+        #                         'total': total,
+        #                     }
+        #                     totales['30'] += treinta
+        #                     totales['mas'] += mas
+        #                     totales['total'] += total
+        #                     facturas_compra.append(pago_dic)
 
         return {'facturas_compra':facturas_compra, 'totales': totales}
 
