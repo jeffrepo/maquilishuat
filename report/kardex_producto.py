743b656e6173b793ca5624388d6d2be80c23cae2
jeffrepo <jeffer.leo93@gmail.com>
2021-03-26 01:28:47 -0600

    test

--- report/kardex_producto.py
+++ report/kardex_producto.py
@@ -45,8 +45,18 @@ class ReportKardexProducto(models.AbstractModel):
 
     def _get_kardex(self,fecha_inicio,fecha_fin,productos_ids):
         logging.warn(productos_ids)
-        movimientos = self.env['stock.move.line'].search([('date','>=', fecha_inicio),('date','<=',fecha_fin),('product_id','in',productos_ids)])
+        movimientos = self.env['stock.move.line'].search([('date','>=', fecha_inicio),('date','<=',fecha_fin),('pickin_id','!=',False),('product_id','in',productos_ids)])
+        movimientos_productos = {}
         if movimientos:
+            for m in movimientos:
+                if m.product_id.id not in movimientos_productos:
+                    movimientos_productos[m.product_id.id] = {'nombre': m.product_id.name,'stock_move_line': []}
+                movimientos_productos[m.product_id.id]['stock_move_line'].append({
+                    'documento': m.reference,
+                    'fecha': m.date,
+                    'proveedor': m.pickin_id.partner_id.name if (m.pickin_id and m.pickin_id.partner_id) else '',
+                    'costo_promedio': 1
+                })
             logging.warn(movimientos)
         return True
 
